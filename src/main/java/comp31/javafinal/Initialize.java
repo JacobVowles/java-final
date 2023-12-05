aea500a6bcf975e4214f404b8730b4e5374f7ff3
Marco <marco.demelo2@student.sl.on.ca>
2023-12-05 15:39:59 -0500

    Marco's Use case Merged

--- src/main/java/comp31/javafinal/Initialize.java
+++ src/main/java/comp31/javafinal/Initialize.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Component;
 
 import comp31.javafinal.model.entities.CustomerAccount;
 import comp31.javafinal.model.entities.Order;
+import comp31.javafinal.model.entities.Products;
 import comp31.javafinal.model.repos.CustomerRepo;
 import comp31.javafinal.model.repos.OrderRepo;
+import comp31.javafinal.model.repos.ProductsRepo;
 
 import com.github.javafaker.Faker;
 
@@ -18,10 +20,12 @@ import org.springframework.boot.CommandLineRunner;
 public class Initialize implements CommandLineRunner {
     CustomerRepo customerRepository;
     OrderRepo orderRepo;
+    ProductsRepo productsRepo;
 
-    public Initialize(CustomerRepo customerRepository, OrderRepo orderRepo) {
+    public Initialize(CustomerRepo customerRepository, OrderRepo orderRepo, ProductsRepo productsRepo) {
         this.customerRepository = customerRepository;
         this.orderRepo = orderRepo;
+        this.productsRepo = productsRepo;
     }
 
     //Faker being used for easy default data
@@ -44,5 +48,8 @@ public class Initialize implements CommandLineRunner {
         orderRepo.save(new Order( 1, "Cake", "Chocolate", "Please add balloons and happy birthday", "Incomplete" ));
         orderRepo.save(new Order( 2, "Cake", "Vanilla", "Orange icing and pumpkin drawings", "Incomplete" ));
         orderRepo.save(new Order( 3, "Cake", "Red Velvet", "Hearts and flowers drawn on the cake please", "Complete" ));
+        productsRepo.save(new Products("Baguette", "Made from the french", 7, 3.50));
+        productsRepo.save(new Products("French Toast", "French Toast in the Morning", 2, 4.00));
+        productsRepo.save(new Products("White Bread", "Plain", 4, 0.10));
     }
 }
